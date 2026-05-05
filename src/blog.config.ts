4ebfb3fdc0647101055ae79f866627024f1818ef
zlym091523 <941783768@qq.com>
2026-05-05 18:10:08 +0800

    更新博客配置，改成我的信息Update blog configuration details
    Updated blog configuration with new title, author, and description. Modified social media links and navigation bar name.

--- src/blog.config.ts
+++ src/blog.config.ts
@@ -1,14 +1,15 @@
 import type { SiteConfig } from "@/features/config/site-config.schema";
 
 export const blogConfig = {
-  title: "站点名称",
-  author: "作者",
+  title: "ShawYoungの博客",
+  author: "Yoviz-Design Studio",
   description:
-    "这是我的个人网站和博客。在这里，我主要分享与技术和生活相关的内容。欢迎阅读！",
+    "在信息过载的时代，真正有价值的不是知识本身，而是被验证过的方法。Yoviz Design Studio是一个专注于技术分享与数字实践的内容平台，聚焦前端开发、AI工具应用、网站搭建以及新媒体与副业探索。如果你正在寻找一个既有深度又有实用性的学习平台，这里会是一个长期值得收藏的地方！",
   social: [
-    { platform: "github", url: "https://github.com/example" },
-    { platform: "email", url: "mailto:example@email.com" },
+    { platform: "github", url: "https://github.com/zlym091523" },
+    { platform: "email", url: "2739560891aa@gmail.com" },
     { platform: "rss", url: "/rss.xml" },
+    { platform: "wechat", url: "Sy-Yoviz_design777" },
   ],
   icons: {
     faviconSvg: "/favicon.svg",
@@ -20,7 +21,7 @@ export const blogConfig = {
   },
   theme: {
     default: {
-      navBarName: "导航栏名称",
+      navBarName: "Shaw Young的博客",
     },
     fuwari: {
       homeBg: "/images/home-bg.webp",
